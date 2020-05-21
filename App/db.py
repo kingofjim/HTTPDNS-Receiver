855ebb3ce79c2fea533c7bc72c9b8763b48e472c
Jim Chang <bill3975598@gmail.com>
2020-05-21 17:00:22 +0800

    remove zero fill

--- App/db.py
+++ App/db.py
@@ -24,6 +24,6 @@ def check_table_exist(tablename):
 
 def create_table_report(table_name):
     with connection.cursor() as cursor:
-        query = "CREATE TABLE `%s` (`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,`ip` varchar(20) NOT NULL DEFAULT '',`domain` varchar(40) NOT NULL DEFAULT '',`query` varchar(255) DEFAULT NULL,`response_time` smallint(5) unsigned zerofill NOT NULL,`datetime` datetime DEFAULT  NULL,`created_at` datetime DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;" % table_name
+        query = "CREATE TABLE `%s` (`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,`ip` varchar(20) NOT NULL DEFAULT '',`domain` varchar(40) NOT NULL DEFAULT '',`query` varchar(255) DEFAULT NULL,`response_time` smallint(5) unsigned NOT NULL,`datetime` datetime DEFAULT  NULL,`created_at` datetime DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;" % table_name
         # print(query)
         cursor.execute(query)
