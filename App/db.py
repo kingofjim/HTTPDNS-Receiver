402609f6f5e6b947d696a3dc00170b12919cc003
Jim Chang <bill3975598@gmail.com>
2020-06-12 17:56:33 +0800

    fix bugs

--- App/db.py
+++ App/db.py
@@ -26,9 +26,9 @@ def check_table_exist(tablename):
 
 def create_table_report(table_name):
     with connection.cursor() as cursor:
-        if table_name.find('_hqt_'):
+        if table_name.find('_hqt_') > -1:
             query = "CREATE TABLE `%s` (`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,`ip` varchar(20) NOT NULL DEFAULT '',`domain` varchar(40) NOT NULL DEFAULT '',`query` varchar(255) DEFAULT NULL,`response_time` smallint(5) unsigned NOT NULL,`datetime` datetime DEFAULT  NULL,`created_at` datetime DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;" % table_name
-        elif table_name.find('_ie_'):
+        elif table_name.find('_ie_') > -1:
             query = "CREATE TABLE `%s`( `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT, `ip` varchar(20) NOT NULL DEFAULT '', `domain` varchar(40) NOT NULL DEFAULT '', `query` varchar(255) DEFAULT NULL, `response_time` smallint(5) unsigned NOT NULL, `device` varchar(10) DEFAULT NULL, `user_agent` varchar(255) DEFAULT NULL, `datetime` datetime DEFAULT NULL, `created_at` datetime DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;" % table_name
         # print(query)
         cursor.execute(query)
